112f666c89526a0d980596c73cebc882418b5174
Unknown <a.reza.sanatjoo@gmail.com>
2018-01-23 00:12:28 +0330

    daram pas zamine va setare haro dorost mikonam

--- main.cpp
+++ main.cpp
@@ -11,6 +11,16 @@
 
 using namespace std;
 
+struct things
+{
+    int x;
+    int y;
+    int r;//radiance
+    bool ingame;
+    int xv;//x's velocity
+    int yv;//y's velocity
+}stars[100];
+
 int main(int argc ,char * args[])
 {
     int screenwidth;
@@ -18,6 +28,11 @@ int main(int argc ,char * args[])
     srand(time(0));
     SDL_Init(SDL_INIT_EVERYTHING);
     SDL_Surface* screen = SDL_SetVideoMode(screenwidth,screenhieght,32,0);
+    while(true)
+    {
+        boxRGBA(screen , 0 , 0 , screenwidth , screenhieght , 0 , 0 , 100 , 250);
+
+    }
 
     return 0;
 }
