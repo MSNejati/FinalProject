e6e1bab9d35caebb5d143e93b41ba5b8ff21c7af
Unknown <a.reza.sanatjoo@gmail.com>
2018-01-26 21:01:32 +0330

    move ro taghriban zadam

--- spaceship_Move.h
+++ spaceship_Move.h
@@ -7,16 +7,31 @@ void our_spaceship_v_initialize(float * our_spaceship_right_v , float * our_spac
     SDL_Event our_spaceship_move;
     if(SDL_PollEvent(&our_spaceship_move))
     {
-        if(keystates[SDLK_RIGHT] && *(our_spaceship_right_v) <= 5)
+        if(keystates[SDLK_RIGHT] /* *(our_spaceship_right_v) <= 5*/)
         {
-            *our_spaceship_right_v += 1;
+            if( *(our_spaceship_left_v) > 0 )
+            {
+                *our_spaceship_left_v = 0;
+            }
+            else
+            {
+                *our_spaceship_right_v += 1;
+            }
         }
-        if(keystates[SDLK_LEFT] && *(our_spaceship_left_v) <= 5)
+        if(keystates[SDLK_LEFT]  /* *(our_spaceship_left_v) <= 5*/)
         {
-                *our_spaceship_left_v += 1;
+            if(*(our_spaceship_right_v) > 0)
+            {
+                *our_spaceship_right_v = 0;
+            }
+            else
+            {
+            *our_spaceship_left_v += 1;
+            }
         }
     }
-    else
+}
+    /*else
     {
          if(*(our_spaceship_left_v) >= 1)
             {
@@ -34,8 +49,8 @@ void our_spaceship_v_initialize(float * our_spaceship_right_v , float * our_spac
             {
                *our_spaceship_right_v = 0;
             }
-    }
-}
+    }*/
+
 
 
 void our_spaceship_move (int * our_spaceshipx ,float * our_spaceship_right_v, float * our_spaceship_left_v)
