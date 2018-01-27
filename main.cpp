c860e99140fd888f8295d4eda4916aeb09ca63ee
Unknown <a.reza.sanatjoo@gmail.com>
2018-01-27 15:41:28 +0330

    tir ha ro taghir dadam bebin chetoure

--- main.cpp
+++ main.cpp
@@ -19,6 +19,7 @@ using namespace std;
 int main(int argc ,char * args[])
 {
     Uint8 *keystates = SDL_GetKeyState( NULL );
+    int arrow_delay = 0;
     int arrow_number = 0; // arrow's number that after pressing the space key throw
     int screenwidth = 1000;
     int screenhieght = 1000;
@@ -57,8 +58,16 @@ int main(int argc ,char * args[])
         sensors_position(screen ,our_spaceshipx ,our_spaceshipy);
 
 	if (keystates[SDLK_SPACE]) {
-		make_arrow_ingame(screen, our_spaceshipx, our_spaceshipy, arrow_number);
-		arrow_number++;
+        if(arrow_delay % 10 ==0)
+        {
+            make_arrow_ingame(screen, our_spaceshipx, our_spaceshipy, arrow_number);
+            arrow_number++;
+            arrow_delay = 1;
+		}
+		else
+		{
+            arrow_delay++;
+		}
 	}
 
 	cout << arrow_number << '\n';
--- throw_arrows.h
+++ throw_arrows.h
@@ -17,25 +17,28 @@ struct shot {
 	bool ingame;
 }arrow[100000];
 
-void make_arrow_ingame (SDL_Surface *screen, int spaceshipx, int spaceshipy, int arrow_number) {
-	
-	arrow[arrow_number].ingame = true;
-	arrow[arrow_number].x = spaceshipx + 50;
-	arrow[arrow_number].y = spaceshipy;
-	filledCircleRGBA(screen, arrow[arrow_number].x, arrow[arrow_number].y, 5, 255, 200, 200, 255);   		
-	
+void make_arrow_ingame (SDL_Surface *screen, int spaceshipx, int spaceshipy, int arrow_number ) {
+
+        arrow[arrow_number].ingame = true;
+        arrow[arrow_number].x = spaceshipx + 50;
+        arrow[arrow_number].y = spaceshipy;
+        //filledCircleRGBA(screen, arrow[arrow_number].x, arrow[arrow_number].y, 5, 255, 200, 200, 255);
+        lineRGBA(screen, arrow[arrow_number].x, arrow[arrow_number].y, arrow[arrow_number].x, arrow[arrow_number].y + 4, 255, 255, 255, 255);
+    //filledCircleRGBA(screen, arrow[arrow_number].x, arrow[arrow_number].y, 5, 255, 200, 200, 255);
+
 }
 
 void move_arrow (SDL_Surface *screen, int arrow_number) {
-	
+
 	for (int i = 0; i < arrow_number; i++) {
-		if (arrow[i].y >= 0, arrow[i].ingame == true) {	
+		if (arrow[i].y >= 0, arrow[i].ingame == true) {
 			arrow[i].y = arrow[i].y + velocity;
-			filledCircleRGBA(screen, arrow[i].x, arrow[i].y, 5, 255, 200, 200, 255);   		
-		}	
+			//filledCircleRGBA(screen, arrow[i].x, arrow[i].y, 5, 255, 200, 200, 255);
+            lineRGBA(screen, arrow[i].x, arrow[i].y, arrow[i].x, arrow[i].y + 4, 255, 255, 255, 255);
+		}
 	}
 
 }
- 
+
 
 
