4833ba89ef538d5095d250e706683cd47ff6c923
Unknown <a.reza.sanatjoo@gmail.com>
2018-01-27 21:26:38 +0330

    enemies ro daram  mizanam

--- main.cpp
+++ main.cpp
@@ -13,15 +13,16 @@
 #include "throw_arrows.h"
 #include "spaceship_Move.h"
 #include "sensors.h"
-
+#include "enemies.h"
 using namespace std;
 
 int main(int argc ,char * args[])
 {
-    
+
     int arrow_number = 0; // arrow's number that after pressing the space key throw
+    int enemy_spaceship_delay;
     int screenwidth = 1000;
-    int screenhieght = 1000;
+    int screenheight = 1000;
     int frame = 0;
     int total_stars = 80;// total number of stars that made till now
     int our_spaceshipx = 450;
@@ -30,9 +31,10 @@ int main(int argc ,char * args[])
     float our_spaceship_left_v = 0;
 
     srand(time(0));
+    enemy_spaceship_delay = (rand() % 20) + 100;
     SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Surface* screen = SDL_SetVideoMode(screenwidth,screenhieght,32,0);
-    first_space_draw(total_stars , screenhieght , screenwidth);
+    SDL_Surface* screen = SDL_SetVideoMode(screenwidth,screenheight,32,0);
+    first_space_draw(total_stars , screenheight , screenwidth);
     SDL_Surface* our_spaceship = load_image("our_spaceship.png");
     apply_surface(our_spaceshipx , our_spaceshipy , our_spaceship, screen);
     SDL_WM_SetCaption( "Star Wars", NULL );
@@ -40,27 +42,33 @@ int main(int argc ,char * args[])
     while(true)
     {
 
-        boxRGBA(screen , 0 , 0 , screenwidth , screenhieght , 0 , 0 , 50 , 250);
+        boxRGBA(screen , 0 , 0 , screenwidth , screenheight , 0 , 0 , 50 , 250);
 
         make_new_star_line(total_stars , screenwidth);
 
-        stars_y_change(total_stars , screenwidth , screenhieght);
+        stars_y_change(total_stars , screenwidth , screenheight);
 
         draw_stars(screen , total_stars);
 
         our_spaceship_v_initialize(&our_spaceship_right_v , &our_spaceship_left_v);
 
-        our_spaceship_move ( &our_spaceshipx , &our_spaceship_right_v, &our_spaceship_left_v ,screenwidth , screenhieght);
+        our_spaceship_move ( &our_spaceshipx , &our_spaceship_right_v, &our_spaceship_left_v ,screenwidth , screenheight);
 
         apply_surface(our_spaceshipx, our_spaceshipy, our_spaceship, screen);
 
         sensors_position(screen ,our_spaceshipx ,our_spaceshipy);
 
-	arrow_number = make_arrow_ingame(screen, our_spaceshipx, our_spaceshipy, arrow_number);
+        arrow_number = make_arrow_ingame(screen, our_spaceshipx, our_spaceshipy, arrow_number);
 
-	move_arrow(screen, arrow_number);
-        
-	SDL_Flip(screen);
+        move_arrow(screen, arrow_number);
+
+        enemy_spaceships_start_pos(screenwidth ,&enemy_spaceship_delay);
+
+        enemy_spaceships_move(screenheight);
+
+        show_enemy_spaceships(screen);
+
+        SDL_Flip(screen);
 
         frame++;
 
