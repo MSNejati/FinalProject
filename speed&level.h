1679d960a85769c0a33ae08813fde3a367d1636f
Unknown <a.reza.sanatjoo@gmail.com>
2018-02-04 19:23:24 +0330

    correction

--- speed&level.h
+++ speed&level.h
@@ -18,9 +18,9 @@ void speed_change(int comm)
         )
         for(int i = 0 ; i < 100 ; i++)
         {
-            if(enemy_arrow[j].ingame)
+            if(enemy_arrow[i].ingame)
             {
-                enemy_arrow[j].y_velocity += 0.5;
+                enemy_arrow[i].y_velocity += 0.5;
             }
         }
     }
